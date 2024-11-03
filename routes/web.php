577e7ec44089ed80e92931e054571ca59ee0a8d4
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2024-11-03 02:45:52 +0000

    Welcome page changed.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+        'isLoggedIn' => Auth::check(),
+        'userName' => auth()->user()->name ?? 'User',
     ]);
 });
 
